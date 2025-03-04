8c07d532a657dabd3c751be629cf1582dc4118db
mathe <mathe@MATHEUS>
2025-03-04 12:04:04 -0300

    Add project files.

---
+++ Dima.Core/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Dima.Core.Models
+{
+    public class Category : Model
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
---
+++ Dima.Core/Models/Model.cs
@@ -0,0 +1,7 @@
+﻿namespace Dima.Core.Models
+{
+    public abstract class Model
+    {
+        public long Id { get; set; }
+    }
+}
